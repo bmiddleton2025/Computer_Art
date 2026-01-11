5665bd9b64ef7dfe16e341ea7e98bb72f4c0c298
Brogoming <kotag@outlook.com>
2026-01-11 14:53:37 -0600

    carousel not working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,54 @@
 <script>
-	import { Carousel } from 'flowbite-svelte';
+	import { expoInOut } from 'svelte/easing';
+	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+
 	const images = [
-		{ src: '/images/MorseCodeImage.png', title: 'MorseCodeImage' },
-		{ src: '/images/MorseCodeImage-1.png', title: 'MorseCodeImage11' }
+		{ id: 1, src: '/images/MorseCodeImage.png', title: 'MorseCodeImage' },
+		{ id: 2, src: '/images/MorseCodeImage-1.png', title: 'MorseCodeImage11' }
 	];
+
+	let currentId = 1;
+
+	function waitTime(seconds) {
+		return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+	}
+
+	onMount(async () => {
+		while (true) {
+			await waitTime(3);
+			currentId = currentId === images.length ? 1 : currentId + 1;
+		}
+	});
 </script>
 
-<div class="flex min-h-screen w-full flex-col items-center justify-center gap-8">
+<div class="static flex min-h-screen w-full flex-col items-center justify-center">
 	<div>
-		<h1 class="text-3xl font-bold">Welcome to Mrs. Middleton's Corner</h1>
+		<h1 class="absolute inset-x-0 top-30 mx-auto w-full text-center text-5xl font-bold">
+			Welcome to Mrs. Middleton's Corner
+		</h1>
 	</div>
-	<div class="w-2/3">
-		<Carousel class="flex justify-center" {images} duration={3000} />
+
+	<div id="default-carousel" class="relative w-full" data-carousel="slide">
+		<div class="rounded-base relative h-56 overflow-hidden md:h-96">
+			{#each images as image}
+				{#if image.id == currentId}
+					<div class="hidden duration-700 ease-in-out" data-carousel-item>
+						<img
+							src={image.src}
+							alt={image.title}
+							class="absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
+						/>
+					</div>
+				{/if}
+			{/each}
+		</div>
 	</div>
+	<!-- <div class="flex w-full justify-center">
+		{#each images as image}
+			{#if image.id == currentId}
+				<img src={image.src} alt={image.title} transition:fade={{ easing: expoInOut }} />
+			{/if}
+		{/each}
+	</div> -->
 </div>
