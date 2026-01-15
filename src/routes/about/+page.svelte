40523a122a4830471df40afd40571e96f542fc45
Brogoming <kotag@outlook.com>
2026-01-14 20:28:21 -0600

    changed some styling

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,27 +10,29 @@
 <div class="space-y-10">
 	<div class="space-y-8">
 		<h1 class="text-6xl font-bold">Creators</h1>
-		<div class="gap-20 space-y-2 lg:grid lg:grid-cols-2 lg:space-y-0">
-			<div class="text-center">
+		<div
+			class="items-center justify-around gap-20 space-y-2 lg:flex lg:grid lg:grid-cols-2 lg:space-y-0"
+		>
+			<div class="mx-auto w-2/3 text-center lg:w-1/2">
 				<img
 					alt="Bobbi Middleton"
 					src="bobbi-middleton.jpg"
-					class="mx-auto w-1/4 rounded-full border-2 border-black object-contain"
+					class="mx-auto w-1/2 rounded-full border-2 border-black object-contain"
 				/>
-				<div>
-					<h2 class="flex-none font-semibold">Project Concept:</h2>
-					<h2 class="flex-none">Bobbi Middleton</h2>
+				<div class="text-sm lg:text-base">
+					<h2 class="font-semibold">Project Concept:</h2>
+					<h2>Bobbi Middleton</h2>
 				</div>
 			</div>
-			<div class="text-center">
+			<div class="mx-auto w-2/3 text-center lg:w-1/2">
 				<img
 					alt="Dakota Gullicksen"
 					src="dakota2025.jpg"
-					class="mx-auto w-1/4 rounded-full border-2 border-black object-contain"
+					class="mx-auto w-1/2 rounded-full border-2 border-black object-contain"
 				/>
-				<div>
-					<h2 class="flex-none font-semibold">Software Developer:</h2>
-					<h2 class="flex-none">Dakota Gullicksen</h2>
+				<div class="text-sm lg:text-base">
+					<h2 class="font-semibold">Software Developer:</h2>
+					<h2>Dakota Gullicksen</h2>
 				</div>
 			</div>
 		</div>
