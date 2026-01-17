1c7311a379f6b1257064465b144fae0fc48382e3
Brogoming <kotag@outlook.com>
2026-01-17 08:58:34 -0600

    try now

--- build/_app/immutable/chunks/DRLfUOtH.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_ba6zzv?.base??"/Computer_Art",a=globalThis.__sveltekit_ba6zzv?.assets??s??"";export{a,s as b};
---
+++ build/_app/immutable/chunks/VeRZ8jLS.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1votpug?.base??"/Computer_Art",t=globalThis.__sveltekit_1votpug?.assets??s??"";export{t as a,s as b};
