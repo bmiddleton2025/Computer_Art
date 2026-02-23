e45f4a2f572de23f5dd841c0cb0f758c5305eb0c
Brogoming <kotag@outlook.com>
2026-02-22 22:32:16 -0600

    run build

---
+++ build/_app/immutable/entry/start.CCXG3hVa.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/VdMIknrX.js";import{x as t}from"../chunks/DHTnUW11.js";export{t as load_css,r as start};
--- src/lib/components/ComputerNav.svelte
+++ src/lib/components/ComputerNav.svelte
@@ -10,7 +10,7 @@
 <div
 	class="fixed flex w-full items-center justify-around space-x-4 bg-zinc-800 px-4 py-3 text-base text-zinc-400 sm:space-x-6 sm:px-6 sm:py-4 md:text-lg lg:text-xl"
 >
-	<a href="{base}/" class={activeUrl == '/' ? activeClass : nonActiveClass}>Home</a>
-	<a href="{base}/how-to-use" class={activeUrl == '/how-to-use' ? activeClass : nonActiveClass}>How To Use</a>
-	<a href="{base}/about" class={activeUrl == '/about' ? activeClass : nonActiveClass}>About</a>
+	<a href="{base}/" class={activeUrl == `${base}/` ? activeClass : nonActiveClass}>Home</a>
+	<a href="{base}/how-to-use" class={activeUrl == `${base}/how-to-use` ? activeClass : nonActiveClass}>How To Use</a>
+	<a href="{base}/about" class={activeUrl == `${base}/about` ? activeClass : nonActiveClass}>About</a>
 </div>
